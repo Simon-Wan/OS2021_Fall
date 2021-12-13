16ecde682e630e086f0fa54e5aa85e5095868294
Simon-Wan <63910095+Simon-Wan@users.noreply.github.com>
2021-12-13 14:39:59 +0800

    proj3 q1 temp

--- Memory/lib/array_list.cc
+++ Memory/lib/array_list.cc
@@ -4,12 +4,21 @@
 
 namespace proj3 {
     ArrayList::ArrayList(size_t sz, MemoryManager* cur_mma, int id){
+        this->size = sz;
+        this->mma = cur_mma;
+        this->array_id = id;
     }
     int ArrayList::Read (unsigned long idx){
         //read the value in the virtual index of 'idx' from mma's memory space
+        unsigned vir_page_num = idx / PageSize;
+        unsigned vir_page_offset = idx % PageSize;
+        return this->mma->ReadPage(this->array_id, vir_page_num, vir_page_offset);
     }
     void ArrayList::Write (unsigned long idx, int value){
         //write 'value' in the virtual index of 'idx' into mma's memory space
+        unsigned vir_page_num = idx / PageSize;
+        unsigned vir_page_offset = idx % PageSize;
+        this->mma->WritePage(this->array_id, vir_page_num, vir_page_offset, value);
     }
     ArrayList::~ArrayList(){
     }
